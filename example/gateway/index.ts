68909c1f3ec9b518787879ae4e875ef6d577640c
Aex <spryti@qq.com>
2021-05-11 19:43:55 +0800

    Add develope example

---
+++ example/gateway/index.ts
@@ -0,0 +1,8 @@
+import { CoaEnv } from 'coa-env'
+import { CoaContext, CoaHttp } from '../../lib'
+
+export const http = new CoaHttp(CoaContext, new CoaEnv('1.0.0'), { baseUrl: '/api/' })
+
+http.start().then(() => {
+
+}, () => { })
