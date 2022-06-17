9dab37d19a03be9d44d78e04d759688771ab1503
imswk <syfzxx@vip.qq.com>
2022-06-17 23:22:43 +0800

    feat: add responseError for interceptor

--- lib/service/CoaInterceptor.ts
+++ lib/service/CoaInterceptor.ts
@@ -7,4 +7,7 @@ export class CoaInterceptor {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async response(ctx: CoaContext, result: CoaRouterHandlerResult): Promise<CoaRouterHandlerResult | void> {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async responseError(handlerError: { error: { code: string; message: string } }, rawError: any): Promise<CoaRouterHandlerResult | void> {}
 }
