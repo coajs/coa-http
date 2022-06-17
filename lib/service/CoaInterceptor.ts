c1c6e2b061b727c19a259860c0966e7dc6826462
Aex <adaex.yang@gmail.com>
2022-06-17 14:35:47 +0000

    feat: add response type

--- lib/service/CoaInterceptor.ts
+++ lib/service/CoaInterceptor.ts
@@ -3,10 +3,8 @@ import { CoaRouterHandlerResult } from './CoaRouter'
 
 export class CoaInterceptor {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async request(ctx: CoaContext): Promise<void> {
-    // do something
-  }
+  async request(ctx: CoaContext): Promise<void> {}
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async response(ctx: CoaContext, result: CoaRouterHandlerResult) {}
+  async response(ctx: CoaContext, result: CoaRouterHandlerResult): Promise<CoaRouterHandlerResult | void> {}
 }
